645bf851228525ffdfc4291a6aead2d6fadcc683
AllihP <hillaprincebambe@gmail.com>
2026-04-03 09:15:45 +0100

    V2

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-portfolio-hpb-2025'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
